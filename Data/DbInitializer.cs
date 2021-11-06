e9ac4c72375f250b8edaa884b15b6bc274681c2d
jpraj <R4vW\fY+rO&R1i/l>
2021-11-06 10:27:14 +0100

    database created on start if not exists; initial database with head John Smith

---
+++ Data/DbInitializer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using TrainStation.Models;
+
+namespace TrainStation.Data
+{
+    public static class DbInitializer
+    {
+        public static void Initialize(ApplicationDbContext context)
+        {
+            // Look for any students.
+            if (context.Employee.Any())
+            {
+                return;   // DB has been seeded
+            }
+
+            var employees = new Employee[]
+            {
+                new Employee{ Name = "John", Surname = "Smith", BirthDate = DateTime.Parse("1990-09-01"), Type = "head"},
+            };
+
+            context.Employee.AddRange(employees);
+            context.SaveChanges();
+        }
+    }
+}
